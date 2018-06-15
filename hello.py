94f60c701bc64f9bfa82054aaaed460ad5526784
MayZinThwe <39287919+MayZinThwe@users.noreply.github.com>
2018-06-15 14:01:40 +0630

    change app to myapp

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
 from flask import Flask
 
-app = Flask(__name__)
+myapp = Flask(__name__)
 
-@app.route("/")
+@myapp.route("/")
 def hello():
     return "Hello World"
 
-@app.route("/tiide")
+@myapp.route("/tiide")
 def tiide():
     return "Welcome to TIIDE World"
 
